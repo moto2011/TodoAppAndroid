49962cadc09876c360d35b275ad50fad90865891
Minoru Nakamura <nakamura200701@gmail.com>
2024-11-22 23:52:40 +0900

    Update targetSdk35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
     defaultConfig {
         applicationId = "com.motonaka.todoapp"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "0.1.0"
 
